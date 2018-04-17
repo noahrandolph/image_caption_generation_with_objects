39df7ba954babfa08bf2d8bbd08eae4ee28283ae
Ubuntu <ubuntu@ip-172-31-28-61.ec2.internal>
2018-04-17 06:15:36 +0000

    adding CNN models for plotting word counts

--- train.lua
+++ train.lua
@@ -256,8 +256,8 @@ local function bboxencode2coords(bbox_encode)
   local bbox_coords = torch.Tensor(bbox_encode:size(1), bbox_encode:size(2), 4):typeAs(x):zero()
   bbox_coords[{ {}, {}, 1 }] = x
   bbox_coords[{ {}, {}, 2 }] = y
-  bbox_coords[{ {}, {}, 3 }] = perimeter
-  bbox_coords[{ {}, {}, 4 }] = area
+  bbox_coords[{ {}, {}, 3 }] = width
+  bbox_coords[{ {}, {}, 4 }] = height
   -- 608x608 is the YOLO object detection output dimention
   bbox_coords = bbox_coords:float():div(608)
   return bbox_coords
